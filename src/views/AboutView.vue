cf7e67e3494ed433eed5847e8e40b539a89e0b9a
unknown <G1656790@www.com>
2023-03-21 09:09:16 +0800

    第一次上传

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,28 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts" setup>
+// import { defineComponent } from "vue";
+
+// export default defineComponent({
+//   name: "AboutView",
+//   setup() {
+//     return {};
+//   },
+// });
+interface PerIts {
+  readonly name: string;
+  age?: number;
+  getName: () => void;
+}
+class M implements PerIts {
+  name = "杜";
+  age = 89;
+  getName() {
+    return;
+  }
+}
+let m = new M();
+m.name = "sdjijdis";
+m.age = 22;
+</script>
